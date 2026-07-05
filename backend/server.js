6c85a96f4c7bb2e3302e952826acbef9cbd95f9e
amyweb0 <amydev22@gmail.com>
2026-07-06 00:03:38 +0530

    deploy_without_db.js_update

--- backend/server.js
+++ backend/server.js
@@ -50,8 +50,14 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 
-connectDB().then(() => {
-  app.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}`),
-  );
-});
+if (process.env.VERCEL) {
+  connectDB();
+} else {
+  connectDB().then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`),
+    );
+  });
+}
+
+export default app;
